1e6f5ae2ecaefdf11195240857d5602fe7ea2c31
rodrigandres <rodrigoula19@gmail.com>
2023-12-18 21:44:42 -0300

    Se actualiza ruta URL

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ import authRoutes from './routes/auth.routes.js'
 import googleMapsRoutes from './routes/googleMaps.routes.js'
 
 const PORT = process.env.PORT ?? 3000
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
+
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: FRONTEND_URL, credentials: true }))
 app.use(express.json())
 
 app.get('/', (_, res) => {
